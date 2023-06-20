55a1a78bc309c591b0622fd4fe350c07627dc784
Danward Bautista <danwarddeveloper@gmail.com>
2023-06-20 14:16:23 +0800

    disable auth

--- routes/api.php
+++ routes/api.php
@@ -24,24 +24,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    //Authentication API
-    Route::get('/user', [AuthenticationController::class, 'getCurrentUser']);
-    Route::post('/logout', [AuthenticationController::class, 'logout']);
-
-    //Voucher API
-    Route::get('/example', [VoucherController::class, 'example']);
-    Route::get('/voucher', [VoucherController::class, 'getAllVouchers']);
-    Route::get('/voucher/{voucherCode}', [VoucherController::class, 'getVoucherByCode']);
-    Route::post('/voucher', [VoucherController::class, 'createNewVoucher']);
-    Route::put('/voucher/{voucherCode}', [VoucherController::class, 'editVoucherByCode']);
-    Route::put('/voucher-set-inactive/{voucherCode}', [VoucherController::class, 'setVoucherInactive']);
-
-    //Voucher SOAP API
-    Route::get('/voucher-soap', [SoapVoucherController::class, 'SOAPGetAllVouchers']);
-    Route::get('/voucher-soap/{voucherCode}', [SoapVoucherController::class, 'SOAPGetVoucherByCode']);
-    Route::post('/voucher-soap', [SoapVoucherController::class, 'SOAPCreateNewVoucher']);
+    
 
 });
+
+//Authentication API
+Route::get('/user', [AuthenticationController::class, 'getCurrentUser']);
+Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+//Voucher API
+Route::get('/example', [VoucherController::class, 'example']);
+Route::get('/voucher', [VoucherController::class, 'getAllVouchers']);
+Route::get('/voucher/{voucherCode}', [VoucherController::class, 'getVoucherByCode']);
+Route::post('/voucher', [VoucherController::class, 'createNewVoucher']);
+Route::put('/voucher/{voucherCode}', [VoucherController::class, 'editVoucherByCode']);
+Route::put('/voucher-set-inactive/{voucherCode}', [VoucherController::class, 'setVoucherInactive']);
+
+//Voucher SOAP API
+Route::get('/voucher-soap', [SoapVoucherController::class, 'SOAPGetAllVouchers']);
+Route::get('/voucher-soap/{voucherCode}', [SoapVoucherController::class, 'SOAPGetVoucherByCode']);
+Route::post('/voucher-soap', [SoapVoucherController::class, 'SOAPCreateNewVoucher']);
 
 //Authentication API
 Route::post('/register', [AuthenticationController::class, 'register']);
