f6737c354b95dae5cca822082d8e17e4edb43bce
Danward Bautista <danwarddeveloper@gmail.com>
2023-06-24 23:02:40 +0800

    Mass Voucher Creation

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -6,10 +6,10 @@ use App\Models\VoucherModel;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class VoucherController extends Controller
 {
-    //
     public function example(Request $request)
     {
 
@@ -43,8 +43,7 @@ class VoucherController extends Controller
     {
         $voucher = VoucherModel::where('voucher_code', $voucherCode)->first();
 
-        if(!$voucher)
-        {
+        if (!$voucher) {
             return response([
                 'message' => "Voucher not found",
             ], 404);
@@ -102,12 +101,215 @@ class VoucherController extends Controller
         ], 200);
     }
 
+    public function createNewVoucherMultple(Request $request)
+    {
+        $request->validate([
+            'value' => 'required|integer',
+            'expiry_date' => 'nullable|date_format:Y-m-d',
+            'service_reference' => 'nullable',
+            'voucher_count' => 'required|integer|min:1',
+        ]);
+
+        function generateVoucherCode($length)
+        {
+            $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $code = '';
+            $max = strlen($characters) - 1;
+
+            for ($i = 0; $i < $length; $i++) {
+                $code .= $characters[random_int(0, $max)];
+            }
+
+            return $code;
+        }
+
+        $vouchers = [];
+        $count = $request->voucher_count;
+
+        // Generate unique voucher codes
+        for ($i = 0; $i < $count; $i++) {
+            $voucherCodeExists = true;
+            $voucherCode = '';
+
+            while ($voucherCodeExists) {
+                $voucherCode = generateVoucherCode(16);
+                $voucherCodeExists = VoucherModel::where('voucher_code', $voucherCode)->exists();
+            }
+
+            $voucher = VoucherModel::create([
+                'voucher_code' => $voucherCode,
+                'value' => $request->value,
+                'expiry_date' => $request->expiry_date,
+                'status' => 'active',
+                'service_reference' => $request->service_reference,
+                'created_by' => 1
+            ]);
+
+            $vouchers[] = $voucher;
+        }
+
+        return response([
+            'message' => "Vouchers created successfully",
+            'results' => $vouchers
+        ], 200);
+    }
+
+    public function createNewVoucherArray(Request $request)
+    {
+        $request->validate([
+            '*.value' => 'required|integer',
+            '*.expiry_date' => 'nullable|date_format:Y-m-d',
+            '*.service_reference' => 'nullable',
+            '*.voucher_count' => 'required|integer|min:1',
+        ]);
+
+        function generateVoucherCode($length)
+        {
+            $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $code = '';
+            $max = strlen($characters) - 1;
+
+            for ($i = 0; $i < $length; $i++) {
+                $code .= $characters[random_int(0, $max)];
+            }
+
+            return $code;
+        }
+
+        $vouchers = [];
+
+        foreach ($request->all() as $entry) {
+            $count = $entry['voucher_count'];
+
+            // Generate unique voucher codes
+            for ($i = 0; $i < $count; $i++) {
+                $voucherCodeExists = true;
+                $voucherCode = '';
+
+                while ($voucherCodeExists) {
+                    $voucherCode = generateVoucherCode(16);
+                    $voucherCodeExists = VoucherModel::where('voucher_code', $voucherCode)->exists();
+                }
+
+                $voucher = VoucherModel::create([
+                    'voucher_code' => $voucherCode,
+                    'value' => $entry['value'],
+                    'expiry_date' => $entry['expiry_date'],
+                    'status' => 'active',
+                    'service_reference' => $entry['service_reference'],
+                    'created_by' => 1
+                ]);
+
+                $vouchers[] = $voucher;
+            }
+        }
+
+        return response([
+            'message' => "Vouchers created successfully",
+            'results' => $vouchers
+        ], 200);
+    }
+
+    public function createNewVoucherCSV(Request $request)
+{
+    $request->validate([
+        'file' => 'required|file',
+    ]);
+
+    $file = $request->file('file');
+
+    // Check the file extension
+    $extension = $file->getClientOriginalExtension();
+    if ($extension !== 'csv') {
+        return response([
+            'message' => 'Invalid file format. Only CSV files are supported.',
+        ], 400);
+    }
+
+    $filePath = $file->getPathname();
+    $file = fopen($filePath, 'r');
+
+    // Skip the header row
+    fgetcsv($file);
+
+    function generateVoucherCode($length)
+    {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $code = '';
+        $max = strlen($characters) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $code .= $characters[random_int(0, $max)];
+        }
+
+        return $code;
+    }
+
+    $vouchers = [];
+
+    while (($row = fgetcsv($file)) !== false) {
+        $value = $row[0];
+        $expiry_date = $row[1];
+        $service_reference = $row[2];
+        $voucher_count = $row[3];
+
+        // Validate the data
+        $validator = Validator::make([
+            'value' => $value,
+            'expiry_date' => $expiry_date,
+            'service_reference' => $service_reference,
+            'voucher_count' => $voucher_count,
+        ], [
+            'value' => 'required|integer',
+            'expiry_date' => 'nullable|date_format:Y-m-d',
+            'service_reference' => 'nullable',
+            'voucher_count' => 'required|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response([
+                'message' => 'Invalid data in the file.',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        // Generate unique voucher codes
+        for ($i = 0; $i < $voucher_count; $i++) {
+            $voucherCodeExists = true;
+            $voucherCode = '';
+
+            while ($voucherCodeExists) {
+                $voucherCode = generateVoucherCode(16);
+                $voucherCodeExists = VoucherModel::where('voucher_code', $voucherCode)->exists();
+            }
+
+            $voucher = VoucherModel::create([
+                'voucher_code' => $voucherCode,
+                'value' => $value,
+                'expiry_date' => $expiry_date,
+                'status' => 'active',
+                'service_reference' => $service_reference,
+                'created_by' => 1,
+            ]);
+
+            $vouchers[] = $voucher;
+        }
+    }
+
+    fclose($file);
+
+    return response([
+        'message' => 'Vouchers created successfully',
+        'results' => $vouchers,
+    ], 200);
+}
+
+
     public function editVoucherByCode($voucherCode, Request $request)
     {
         $voucher = VoucherModel::where('voucher_code', $voucherCode)->first();
 
-        if(!$voucher)
-        {
+        if (!$voucher) {
             return response([
                 'message' => "Voucher not found",
             ], 404);
@@ -138,8 +340,7 @@ class VoucherController extends Controller
     {
         $voucher = VoucherModel::where('voucher_code', $voucherCode)->first();
 
-        if(!$voucher)
-        {
+        if (!$voucher) {
             return response([
                 'message' => "Voucher not found",
             ], 404);
--- routes/api.php
+++ routes/api.php
@@ -25,18 +25,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     
-
+    //Authentication API
+    Route::get('/user', [AuthenticationController::class, 'getCurrentUser']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
-
-//Authentication API
-Route::get('/user', [AuthenticationController::class, 'getCurrentUser']);
-Route::post('/logout', [AuthenticationController::class, 'logout']);
 
 //Voucher API
 Route::get('/example', [VoucherController::class, 'example']);
 Route::get('/voucher', [VoucherController::class, 'getAllVouchers']);
 Route::get('/voucher/{voucherCode}', [VoucherController::class, 'getVoucherByCode']);
 Route::post('/voucher', [VoucherController::class, 'createNewVoucher']);
+Route::post('/voucher-multiple', [VoucherController::class, 'createNewVoucherMultple']);
+Route::post('/voucher-array', [VoucherController::class, 'createNewVoucherArray']);
+Route::post('/voucher-file', [VoucherController::class, 'createNewVoucherCSV']);
 Route::put('/voucher/{voucherCode}', [VoucherController::class, 'editVoucherByCode']);
 Route::put('/voucher-set-inactive/{voucherCode}', [VoucherController::class, 'setVoucherInactive']);
 
