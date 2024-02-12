5e921dcb9d0fc7d349dfaf6c2c15a771ca765a23
Danward Bautista <danwarddeveloper@gmail.com>
2024-02-12 11:50:09 +0800

    Require authorization headers

--- app/Http/Middleware/KeycloakValidationMiddleware.php
+++ app/Http/Middleware/KeycloakValidationMiddleware.php
@@ -11,32 +11,37 @@ class KeycloakValidationMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (env('AUTH_ENABLE') === true) {
-            $token = $request->header('Authorization');
-
-            $response = Http::withHeaders(['Authorization' => $token])
-                ->get(env('AUTH_USERINFO_URL'));
-
-            if ($response->successful()) {
-                $keycloakData = $response->json();
-
-                if (isset($keycloakData['preferred_username'])) {
-                    $preferredUsername = $keycloakData['preferred_username'];
-
-                    $request->attributes->add(['preferred_username' => $preferredUsername]);
-                }
-
-                return $next($request);
-            } else {
-                return response([
-                    'message' => 'Error. Token is not valid.',
-                    'keycloak_response' => $response->json(),
-                    'token' => $token,
-                ], $response->status());
-            }
+        if (!$request->hasHeader('Authorization')) {
+            return response()->json(['message' => 'Authorization header is required'], 401);
         } else {
-            $request->attributes->add(['preferred_username' => 'auth-off']);
-            return $next($request);
+
+            if (env('AUTH_ENABLE') === true) {
+                $token = $request->header('Authorization');
+
+                $response = Http::withHeaders(['Authorization' => $token])
+                    ->get(env('AUTH_USERINFO_URL'));
+
+                if ($response->successful()) {
+                    $keycloakData = $response->json();
+
+                    if (isset($keycloakData['preferred_username'])) {
+                        $preferredUsername = $keycloakData['preferred_username'];
+
+                        $request->attributes->add(['preferred_username' => $preferredUsername]);
+                    }
+
+                    return $next($request);
+                } else {
+                    return response([
+                        'message' => 'Error. Token is not valid.',
+                        'keycloak_response' => $response->json(),
+                        'token' => $token,
+                    ], $response->status());
+                }
+            } else {
+                $request->attributes->add(['preferred_username' => 'auth-off']);
+                return $next($request);
+            }
         }
     }
 }
