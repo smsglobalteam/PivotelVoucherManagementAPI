d47511a8e70a9bf1f1355f7615b0fc70f40bbb53
Danward Bautista <danwarddeveloper@gmail.com>
2024-06-19 12:57:36 +0800

    Error codes update

--- app/Http/Controllers/ErrorCodesController.php
+++ app/Http/Controllers/ErrorCodesController.php
@@ -142,187 +142,196 @@ class ErrorCodesController extends Controller
     }
 
     public function mapValidationErrorsToCustomCodes($validator)
-    {
-        $failedRules = $validator->failed();
-        $errorMappings = [];
+{
+    $failedRules = $validator->failed();
+    $errorMappings = [];
 
-        foreach ($failedRules as $field => $rules) {
-            foreach (array_keys($rules) as $rule) {
-                // Directly determine the error code within the loop
-                $key = strtolower($field . '_' . $rule);
-                $errorCode = null;
+    foreach ($failedRules as $field => $rules) {
+        foreach (array_keys($rules) as $rule) {
+            // Directly determine the error code within the loop
+            $key = strtolower($field . '_' . $rule);
+            $errorCode = null;
 
-                switch ($key) {
-                    case 'product_code_required':
-                        $errorCode = '-5002';
-                        break;
-                    case 'product_code_regex':
-                        $errorCode = '-5003';
-                        break;
-                    case 'product_code_unique':
-                        $errorCode = '-5004';
-                        break;
-                    case 'product_id_required':
-                        $errorCode = '-5005';
-                        break;
-                    case 'product_id_integer':
-                        $errorCode = '-5006';
-                        break;
-                    case 'product_id_unique':
-                        $errorCode = '-5007';
-                        break;
-                    case 'product_name_required':
-                        $errorCode = '-5008';
-                        break;
-                    case 'product_name_unique':
-                        $errorCode = '-5009';
-                        break;
-                    case 'supplier_required':
-                        $errorCode = '-5010';
-                        break;
-                    case 'status_required':
-                        $errorCode = '-5011';
-                        break;
-                    case 'status_boolean':
-                        $errorCode = '-5012';
-                        break;
+            switch ($key) {
+                case 'product_code_required':
+                    $errorCode = '-5002';
+                    break;
+                case 'product_code_regex':
+                    $errorCode = '-5003';
+                    break;
+                case 'product_code_unique':
+                    $errorCode = '-5004';
+                    break;
+                case 'product_id_required':
+                    $errorCode = '-5005';
+                    break;
+                case 'product_id_integer':
+                    $errorCode = '-5006';
+                    break;
+                case 'product_id_unique':
+                    $errorCode = '-5007';
+                    break;
+                case 'product_name_required':
+                    $errorCode = '-5008';
+                    break;
+                case 'product_name_unique':
+                    $errorCode = '-5009';
+                    break;
+                case 'supplier_required':
+                    $errorCode = '-5010';
+                    break;
+                case 'status_required':
+                    $errorCode = '-5011';
+                    break;
+                case 'status_boolean':
+                    $errorCode = '-5012';
+                    break;
 
-                        // Batch order processing errors
-                    case 'batch_id_required':
-                        $errorCode = '-6002';
-                        break;
-                    case 'batch_id_string':
-                        $errorCode = '-6003';
-                        break;
-                    case 'batch_id_unique':
-                        $errorCode = '-6004';
-                        break;
-                    case 'batch_count_required':
-                        $errorCode = '-6005';
-                        break;
-                    case 'batch_count_integer':
-                        $errorCode = '-6006';
-                        break;
-                    case 'batch_count_min':
-                        $errorCode = '-6007';
-                        break;
-                    case 'product_id_exists':
-                        $errorCode = '-6009';
-                        break;
-                    case 'file_required':
-                        $errorCode = '-6010';
-                        break;
-                    case 'file_mimes':
-                        $errorCode = '-6011';
-                        break;
-                        //-6012, -6013, -6014 are not present here as they are not validation rules but custom checks
+                // Batch order processing errors
+                case 'batch_id_required':
+                    $errorCode = '-6002';
+                    break;
+                case 'batch_id_string':
+                    $errorCode = '-6003';
+                    break;
+                case 'batch_id_unique':
+                    $errorCode = '-6004';
+                    break;
+                case 'batch_count_required':
+                    $errorCode = '-6005';
+                    break;
+                case 'batch_count_integer':
+                    $errorCode = '-6006';
+                    break;
+                case 'batch_count_min':
+                    $errorCode = '-6007';
+                    break;
+                case 'product_id_exists':
+                    $errorCode = '-6009';
+                    break;
+                case 'file_required':
+                    $errorCode = '-6010';
+                    break;
+                case 'file_mimes':
+                    $errorCode = '-6011';
+                    break;
+                //-6012, -6013, -6014 are not present here as they are not validation rules but custom checks
 
-                        // Voucher processing errors
-                    case 'serial_required':
-                        $errorCode = '-7002';
-                        break;
-                    case 'serial_unique':
-                        $errorCode = '-7003';
-                        break;
-                    case 'puk_required':
-                        $errorCode = '-7004';
-                        break;
-                    case 'puk_unique':
-                        $errorCode = '-7005';
-                        break;
-                    case 'value_integer':
-                        $errorCode = '-7006';
-                        break;
-                    case 'expire_date_date_format':
-                        $errorCode = '-7007';
-                        break;
-                    case 'expire_date_after':
-                        $errorCode = '-7008';
-                        break;
-                    case 'imei_string':
-                        $errorCode = '-7009';
-                        break;
-                    case 'simnarrative_string':
-                        $errorCode = '-7010';
-                        break;
-                    case 'msisdn_string':
-                        $errorCode = '-7011';
-                        break;
-                    case 'simno_string':
-                        $errorCode = '-7012';
-                        break;
-                    case 'imsi_string':
-                        $errorCode = '-7013';
-                        break;
-                    case 'service_reference_string':
-                        $errorCode = '-7014';
-                        break;
-                    case 'business_unit_string':
-                        $errorCode = '-7015';
-                        break;
-                    case 'service_reference_required':
-                        $errorCode = '-7016';
-                        break;
-                    case 'business_unit_required':
-                        $errorCode = '-7017';
-                        break;
-                        // Add other cases as needed
+                // Voucher processing errors
+                case 'serial_required':
+                    $errorCode = '-7002';
+                    break;
+                case 'serial_unique':
+                    $errorCode = '-7003';
+                    break;
+                case 'puk_required':
+                    $errorCode = '-7004';
+                    break;
+                case 'puk_unique':
+                    $errorCode = '-7005';
+                    break;
+                case 'value_integer':
+                    $errorCode = '-7006';
+                    break;
+                case 'expire_date_date_format':
+                    $errorCode = '-7007';
+                    break;
+                case 'expire_date_after':
+                    $errorCode = '-7008';
+                    break;
+                case 'imei_string':
+                    $errorCode = '-7009';
+                    break;
+                case 'simnarrative_string':
+                    $errorCode = '-7010';
+                    break;
+                case 'msisdn_string':
+                    $errorCode = '-7011';
+                    break;
+                case 'simno_string':
+                    $errorCode = '-7012';
+                    break;
+                case 'imsi_string':
+                    $errorCode = '-7013';
+                    break;
+                case 'service_reference_string':
+                    $errorCode = '-7014';
+                    break;
+                case 'business_unit_string':
+                    $errorCode = '-7015';
+                    break;
+                case 'service_reference_required':
+                    $errorCode = '-7016';
+                    break;
+                case 'business_unit_required':
+                    $errorCode = '-7017';
+                    break;
+                // Add other cases as needed
 
-                        // Voucher type processing errors
-                    case 'voucher_code_regex':
-                        $errorCode = '-8002';
-                        break;
-                    case 'voucher_code_required':
-                        $errorCode = '-8003';
-                        break;
-                    case 'voucher_name_required':
-                        $errorCode = '-8004';
-                        break;
-                    case 'voucher_type_id_required':
-                        $errorCode = '-8005';
-                        break;
-                    case 'voucher_type_id_exists':
-                        $errorCode = '-8006';
-                        break;
-                    case 'voucher_code_exists':
-                        $errorCode = '-8007';
-                        break;
-                    case 'voucher_code_unique':
-                        $errorCode = '-8008';
-                        break;
-                    case 'voucher_code_reserved':
-                        $errorCode = '-8009';
-                        break;
+                // Voucher type processing errors
+                case 'voucher_code_regex':
+                    $errorCode = '-8002';
+                    break;
+                case 'voucher_code_required':
+                    $errorCode = '-8003';
+                    break;
+                case 'voucher_name_required':
+                    $errorCode = '-8004';
+                    break;
+                case 'voucher_type_id_required':
+                    $errorCode = '-8005';
+                    break;
+                case 'voucher_type_id_exists':
+                    $errorCode = '-8006';
+                    break;
+                case 'voucher_code_exists':
+                    $errorCode = '-8007';
+                    break;
+                case 'voucher_code_unique':
+                    $errorCode = '-8008';
+                    break;
+                case 'voucher_code_reserved':
+                    $errorCode = '-8009';
+                    break;
 
+                // Alert email group processing errors
+                case 'name_required':
+                    $errorCode = '-9002';
+                    break;
+                case 'email_required':
+                    $errorCode = '-9003';
+                    break;
+                case 'email_email':
+                    $errorCode = '-9004';
+                    break;
+                case 'email_unique':
+                    $errorCode = '-9005';
+                    break;
+                case 'threshold_alert_integer':
+                    $errorCode = '-9006';
+                    break;
 
-                        // Alert email group processing errors
-                    case 'name_required':
-                        $errorCode = '-9002';
-                        break;
-                    case 'email_required':
-                        $errorCode = '-9003';
-                        break;
-                    case 'email_email':
-                        $errorCode = '-9004';
-                        break;
-                    case 'email_unique':
-                        $errorCode = '-9005';
-                        break;
+                default:
+                    // Handle unmapped errors with a default code
+                    $errorCode = '-9999'; // Default error code for unmapped errors
+                    // Optionally, log the unmapped error for later review
+                    // error_log("Unmapped validation error: $key");
+                    break;
+            }
 
-                }
-
-                // Only add to the array if an error code was found
-                if ($errorCode !== null) {
-                    $errorMappings[] = [
-                        'error_code' => $errorCode,
-                        'error_field' => $field,
-                    ];
-                }
+            // Only add to the array if an error code was found
+            if ($errorCode !== null) {
+                $errorMappings[] = [
+                    'error_code' => $errorCode,
+                    'error_field' => $field,
+                ];
             }
         }
-
-        return $errorMappings;
     }
+
+    return $errorMappings;
+}
+
 
 
     public function getErrorMessagesFromCodes(array $errorMappings)
